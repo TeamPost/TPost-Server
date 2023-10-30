e6bb41580d49de6defb84dca86a017cc1d5872eb
endelssmomo <soohyuk96@naver.com>
2023-10-30 15:33:42 +0900

    [build] Environment Settings

---
+++ src/main/java/TPost/TeamBlogging/TeamBloggingApplication.java
@@ -0,0 +1,13 @@
+package TPost.TeamBlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamBloggingApplication.class, args);
+
+	}
+}
---
+++ src/test/java/TPost/TeamBlogging/TeamBloggingApplicationTests.java
@@ -0,0 +1,12 @@
+package TPost.TeamBlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
